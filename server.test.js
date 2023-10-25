1c7e25deee927722e70d5b417b81c80d3ee11f99
Mitchell Hess <97816781+Mitchell-Hess@users.noreply.github.com>
2023-10-24 22:11:48 -0500

    added name and euid

--- server.test.js
+++ server.test.js
@@ -9,31 +9,12 @@ describe('API Endpoints', () => {
   let server;
 
   beforeAll(() => {
-    server = app.listen(3000); // Use a different port for testing
+    server = app.listen(3000);
   });
 
   afterAll((done) => {
     server.close(done);
   });
-
-  /*
-  test('should insert a key into the database', async () => {
-    const key = 'your-test-key';
-    const expiration = Date.now() + 3600; // Set the expiration time
-    const result = await dbOperations.insertKey(key, expiration);
-    expect(result).toBe(true); // Verify that the insertion was successful
-  });
-  
-  test('should retrieve valid keys from the database', async () => {
-    const validKeys = await dbOperations.getValidKeys();
-    expect(validKeys).toEqual(expect.arrayContaining([keyPair.toPEM(true)])); // Ensure the retrieved key is valid
-  });
-  
-  test('should retrieve expired keys from the database', async () => {
-    const expiredKeys = await dbOperations.getExpiredKeys();
-    expect(expiredKeys).toEqual(expect.arrayContaining([expiredKeyPair.toPEM(true)])); // Ensure the retrieved key is expired
-  });
-  */
 
   test('should return 405 Method Not Allowed for non-POST requests to /auth', async () => {
     const response = await request(app).get('/auth');
@@ -44,13 +25,11 @@ describe('API Endpoints', () => {
   test('should return a JWT token for POST request to /auth', async () => {
     const response = await request(app).post('/auth');
     expect(response.statusCode).toBe(200);
-    // Add more specific assertions for the token as needed
   });
 
   test('should return an expired JWT token for POST request to /auth?expired=true', async () => {
     const response = await request(app).post('/auth?expired=true');
     expect(response.statusCode).toBe(200);
-    // Add more specific assertions for the expired token as needed
   });
 
   test('should return 405 Method Not Allowed for non-GET requests to /.well-known/jwks.json', async () => {
@@ -63,14 +42,13 @@ describe('API Endpoints', () => {
     const response = await request(app).get('/.well-known/jwks.json');
     expect(response.statusCode).toBeGreaterThanOrEqual(200);;
     expect(response.body).toHaveProperty('keys');
-    // Add more specific assertions for the JSON response as needed
   });
 
   test('should start the server and listen on the specified port', (done) => {
-    const port = 3000; // The expected port
+    const port = 3000;
     const server = app.listen(port, () => {
       server.close(() => {
-        done(); // Ensure the server starts and closes without errors
+        done(); // Ensures the server starts and closes without errors
       });
     });
   });
